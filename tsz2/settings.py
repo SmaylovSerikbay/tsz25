890ade3692e9dcbdb20dc8bfe3e5f46990434dd3
SmaylovSerikbay <augmented.vr@gmail.com>
2025-07-30 11:59:04 +0500

    Add STATIC_ROOT setting to Django settings for static file management

--- tsz2/settings.py
+++ tsz2/settings.py
@@ -126,6 +126,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
